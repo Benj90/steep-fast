ce433dd1551e01d618e9e7b8824fc18ab0726e3c
Benj90 <ben.johnsonm@gmail.com>
2022-02-03 08:26:51 -0800

    made changes to sketch.js

--- README.txt
+++ README.txt
@@ -13,3 +13,14 @@ Then point your browser at http://localhost:8080/
 
 server info - https://github.com/processing/p5.js/wiki/Local-server
 github video by Daniel Schiffman : https://www.youtube.com/watch?v=BCQHnlnPusY
+
+SSH
+
+https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+
+open a terminal, navigate to your project
+do this if you added new files:
+git add .
+
+git commit -am' commit message goes here'
+git pushhtt
--- sketch.js
+++ sketch.js
@@ -1,94 +1,55 @@
-function setup() {
-createCanvas(800,800);
+  function setup()
+{
+  createCanvas(600, 500);
+  background(255);
 
-}
+  stroke(255);
+  fill(0,0,0);
+ellipse(width/2+50,height/2-45,400,400);
 
-function draw() {
-	background(0,0,0);
-
-fill(235,250,200);
-	rectMode(CENTER);
-rect(width+-400, height+-400, 800, 300);
-
-stroke(255,255,255);
-
-	beginShape();
-vertex(46,60);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
+stroke(255);
+  fill(255,100,0);
+ellipse(width/2+30,height/2-45,400,400);
 
 
-beginShape();
-fill(255,0,0);
-   vertex(46, 60);
-  quadraticVertex(319, 219, 372, 101);
-  bezierVertex(430, 360, 150, 100, 128, 295);
-endShape(CLOSE);
+  const w = width * 0.3;
+  const h = height * 0.5;
 
-beginShape();
-fill(75,0,255);
-vertex(width/2-200,height-100);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
+  stroke(30);
+  strokeWeight(width*0.1);
+  strokeCap(SQUARE);
+  noFill();
+  translate(width/2, height/2);
+  beginShape();
+    vertex(-w-10, 0+10)
+    bezierVertex(-w, -h*2, w, h, w+35, 0-100);
+  endShape();
+
+  noLoop();
+
+  stroke(50,50,255);
+  strokeWeight(width*0.1);
+  strokeCap(SQUARE);
+  noFill();
+  translate(width/12-53, height/4-70);
+  beginShape();
+    vertex(-w-11, 0+10)
+    bezierVertex(-w+10, -h*2, w+15, h, w+65, 0-164);
+  endShape();
+
+  noLoop();
 
 
-beginShape();
-fill(255,75,0);
-vertex(width/2-100,height-100);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
+ stroke(0,0,0);
+  strokeWeight(width*0.1);
+  strokeCap(SQUARE);
+  noFill();
+  translate(width/12-53, height/4-70);
+  beginShape();
+    vertex(-w-13, 0+10)
+    bezierVertex(-w+10, -h*2, w+15, h, w+95, 0-229);
+  endShape();
 
-//front feather
-beginShape();
-fill(100,200,0);
-vertex(width/2,height-100);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
-
-//top hat
-beginShape();
-fill(0,75,255);
-   vertex(46, 60);
-  quadraticVertex(319, 219, 372, 101);
-  bezierVertex(430, 360, 10, 10, 128, 295);
-endShape(CLOSE);
-
-//lower beak
-	beginShape();
-	fill(255,200,0);
-vertex(width/2,height/2);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
-
-//upper beak
-beginShape();
-	fill(255,200,0);
-vertex(width/2+15,height/2);
-quadraticVertex(400,119,472,200);
-bezierVertex(430,360,150,100,147,300);
-endShape(CLOSE);
-
-//head
-	beginShape();
-	fill(0,25,75);
-vertex(width/4-32,height/2);
-quadraticVertex(419,119,472,200);
-bezierVertex(430,360,150,100,147,350);
-endShape(CLOSE);
-
-//outer eye
-beginShape();
-fill(255,255,255);
-const size = width*.05;
-ellipse(250, 300, size, size);
-endShape(CLOSE)
-
-//inner eye
-fill(0,0,0);
-ellipse(250,300,25,25);
+  noLoop();
+  
 }
